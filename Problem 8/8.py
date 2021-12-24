b0dcd2cb6d25dda7eb3b5df2e0ff233774a0143c
mihirbpi <mihir.borkar@gmail.com>
2021-12-23 16:36:59 -0800

    Solved Problem 8

---
+++ Problem 8/8.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+input =  list(map(int, list("".join(open("input.txt").read().split("\n")))))
+
+
+
+
+highest_prod = 1
+
+for i in range(0, len(input) - 13):
+
+    print(i)
+    prod_adjacent_digits = np.prod(input[i:i+13])
+
+    if(prod_adjacent_digits > highest_prod):
+        highest_prod = prod_adjacent_digits
+
+print(highest_prod)
