e6ca26d8505e077a96ce8f61d52c2685ea32ac1f
mihirbpi <mihir.borkar@gmail.com>
2021-12-25 13:03:24 -0800

    improve 1

--- Problem 1/1.py
+++ Problem 1/1.py
@@ -11,10 +11,7 @@ for i in range(1, 1000):
     if(i % 3 == 0):
         sum += i
 
-    if(i % 5 == 0):
+    elif(i % 5 == 0):
         sum += i
 
-    if(i % 15 == 0):
-        sum -= i
-        
 print(sum)
