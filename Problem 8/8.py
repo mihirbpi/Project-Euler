19e7fe5f8303f0bdafe78e5d996c619df9a35884
mihirbpi <mihir.borkar@gmail.com>
2021-12-25 12:30:49 -0800

    formatting

--- Problem 8/8.py
+++ Problem 8/8.py
@@ -1,15 +1,9 @@
-
 import numpy as np
+
 input =  list(map(int, list("".join(open("input.txt").read().split("\n")))))
-
-
-
-
 highest_prod = 1
 
 for i in range(0, len(input) - 13):
-
-    print(i)
     prod_adjacent_digits = np.prod(input[i:i+13])
 
     if(prod_adjacent_digits > highest_prod):
